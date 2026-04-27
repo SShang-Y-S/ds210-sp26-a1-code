1f707328e22407d49ce398eca6f70d00e047f623
Shay Sun <shangqjss@gmail.com>
2026-04-26 22:55:53 -0400

    implemented alpha beta pruning

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -123,7 +123,7 @@ fn heuristic(board: &mut Board) -> i32{
 //     return None;
 // }
 
-fn minmax_depth(board: &mut Board, player: Player, depth: u32, max_depth:u32) -> (i32, usize, usize){
+fn minmax_depth(board: &mut Board, player: Player, depth: u32, max_depth: u32, mut alpha: i32, mut beta: i32) -> (i32, usize, usize){
     if board.game_over(){
         return (board.score(), 0, 0)
     }
@@ -134,32 +134,54 @@ fn minmax_depth(board: &mut Board, player: Player, depth: u32, max_depth:u32) ->
 
     let moves: Vec<(usize, usize)> = board.moves();
     let mut best_move = moves[0];
-    board.apply_move(best_move, player);
-    let (mut best_score, _, _) = minmax_depth(board, player.flip(), depth+1, max_depth);
-    board.undo_move(best_move, player);
-
-    for i in 1..moves.len(){
-            let player_move = moves[i];
-            board.apply_move(player_move, player);
-            let (score, _, _) = minmax_depth(board, player.flip(), depth+1, max_depth);
-            board.undo_move(player_move, player);
 
             match player{
                 Player::X => {
-                    if score > best_score{ 
-                        best_move = player_move;
-                        best_score = score;
-                    }
-                }
-                Player::O => {
-                    if score < best_score{
-                        best_move = player_move;
-                        best_score = score;
+                    let mut best_score = i32::MIN;
+                    for m in moves{
+                        board.apply_move(m, player);
+                        let (score, _, _) = minmax_depth(board, player.flip(), depth+1, max_depth, alpha, beta);
+                        board.undo_move(m, player);
+
+                        if score > best_score{ 
+                            best_score = score;
+                            best_move = m;
+
+                        }
+
+                        if best_score > alpha{
+                            alpha = best_score;
+                        }
+
+                        if alpha >= beta{
+                            break;
                         }
                     }
+                    return (best_score, best_move.0, best_move.1);
                 }
-        }
-        return (best_score, best_move.0 as usize, best_move.1 as usize);
+                Player::O => {
+                    let mut best_score = i32::MAX;
+
+                    for m in moves{
+                        board.apply_move(m, player);
+                        let (score, _, _) = minmax_depth(board, player.flip(), depth + 1, max_depth, alpha, beta);
+                        board.undo_move(m, player);
+                        if score < best_score{
+                            best_score = score;
+                            best_move = m;
+                            }
+                        if best_score < beta {
+                            beta = best_score;
+                        }
+                        if alpha >= beta{
+                            break;
+                        }
+                        
+                    }
+                    return (best_score, best_move.0 as usize, best_move.1 as usize);
+
+                }
+            }
     }
 // Put your solution here.
 impl Agent for SolutionAgent {
@@ -179,6 +201,6 @@ impl Agent for SolutionAgent {
         // else{
         //     max_depth = 4;
         // }
-        return minmax_depth(board, player, 0, max_depth);
+        return minmax_depth(board, player, 0, max_depth, i32::MIN, i32::MAX);
     }
 }
